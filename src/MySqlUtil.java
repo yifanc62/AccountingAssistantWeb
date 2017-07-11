15807bd5efa9ef916a33a37db8d83eb2e227e56a
Yifan Cui <yifanc62@qq.com>
2017-07-11 10:21:50 +0800

    init

---
+++ src/MySqlUtil.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Yifan on 2017/7/11.
+ * MySQL Util Class
+ *
+ * @author Yifan Cui
+ * @version 1.0.0
+ */
+public class MySqlUtil {
+    public static void connect(String url) {
+
+    }
+}
